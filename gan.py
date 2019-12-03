e4dbb2014df7e14600074084889d311b6a86670e
Ruoyi Wang <endvroy@gmail.com>
2019-12-02 22:32:25 -0500

    further cleaning up

--- gan.py
+++ gan.py
@@ -126,12 +126,12 @@ def gan_train():
             b_size = real_cpu.size(0)
             label = torch.full((b_size,), real_label, device=device)
             # Forward pass real batch through D
-            output = disc_net(real_cpu).view(-1)
+            disc_output = disc_net(real_cpu).view(-1)
             # Calculate loss on all-real batch
-            errD_real = bce_loss(output, label)
+            disc_loss_real = bce_loss(disc_output, label)
             # Calculate gradients for D in backward pass
-            errD_real.backward()
-            D_x = output.mean().item()
+            disc_loss_real.backward()
+            D_x = disc_output.mean().item()
 
             ## Train with all-fake batch
             # Generate batch of latent vectors
@@ -140,14 +140,14 @@ def gan_train():
             fake = gen_net(noise)
             label.fill_(fake_label)
             # Classify all fake batch with D
-            output = disc_net(fake.detach()).view(-1)
+            disc_output = disc_net(fake.detach()).view(-1)
             # Calculate D's loss on the all-fake batch
-            errD_fake = bce_loss(output, label)
+            disc_loss_fake = bce_loss(disc_output, label)
             # Calculate the gradients for this batch
-            errD_fake.backward()
-            D_G_z1 = output.mean().item()
+            disc_loss_fake.backward()
+            D_G_z1 = disc_output.mean().item()
             # Add the gradients from the all-real and all-fake batches
-            errD = errD_real + errD_fake
+            disc_loss = disc_loss_real + disc_loss_fake
             # Update D
             disc_optimizer.step()
 
@@ -157,12 +157,13 @@ def gan_train():
             gen_net.zero_grad()
             label.fill_(real_label)  # fake labels are real for generator cost
             # Sigan_args.nce we just updated D, perform another forward pass of all-fake batch through D
-            output = disc_net(fake).view(-1)
+            gen_output = disc_net(fake).view(-1)
             # Calculate G's loss based on this output
-            errG = bce_loss(output, label)
+            gen_loss = bce_loss(gen_output, label)
             # Calculate gradients for G
-            errG.backward()
-            D_G_z2 = output.mean().item()
+            gen_loss.backward()
+            #for logging only
+            D_G_z2 = gen_output.mean().item()
             # Update G
             gen_optimizer.step()
 
@@ -170,11 +171,11 @@ def gan_train():
             if i % 50 == 0:
                 print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
                       % (epoch, gan_args.epochs, i, len(dataloader),
-                         errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+                         disc_loss.item(), gen_loss.item(), D_x, D_G_z1, D_G_z2))
 
             # Save Losses for plotting later
-            G_losses.append(errG.item())
-            D_losses.append(errD.item())
+            G_losses.append(gen_loss.item())
+            D_losses.append(disc_loss.item())
 
             # Check how the generator is doing by saving G's output on fixed_noise
             if (i % 500 == 0) or ((epoch == gan_args.epochs - 1) and (i == len(dataloader) - 1)):
